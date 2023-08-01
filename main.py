e6c5f57ee16709fec5a5faba5462702ea5334ba9
Wesam Nabeel <wesamezio@gmail.com>
2023-08-02 02:43:20 +0300

    extract images with alpha channel, higher resolution, add gradient background

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 from pdfCropMargins import crop
-from pdf2image import convert_from_path
-import os
 
-def pdf_to_images(pdf_path, output_folder):
-    # Ensure the output folder exists
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    # Convert the PDF to a list of PIL images
-    images = convert_from_path(pdf_path,dpi=300)
-
-    # Save each image to the output folder
-    for i, image in enumerate(images):
-        image.save(os.path.join(output_folder, f'page_{i}.png'), 'PNG')
+import fitz  # PyMuPDF library
+from PIL import Image
+import numpy as np
 
 
+def extract_images_from_pdf(pdf_file, output_dir):
+    pdf_document = fitz.open(pdf_file)
 
-crop(["-o","meow_output.pdf","-p","0","-a","-5", "test.pdf"])
+    for page_number in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_number)
+        image = page.get_pixmap(matrix=fitz.Matrix(1, 1), dpi=300, alpha=True)
+        image.save(f"{output_dir}/page{page_number + 1}.png")
+
+    pdf_document.close()
+
+
+def overlay_images(foreground_path, output_path):
+    foreground = Image.open(foreground_path)
+
+    gradient = np.linspace((200, 200, 200), (100, 100, 100), 900)[np.newaxis, :, :]
+    background = Image.fromarray(np.uint8(gradient), 'RGB')
+
+    background = background.resize(foreground.size, Image.ADAPTIVE)
+
+    merged_image = Image.alpha_composite(background.convert('RGBA'), foreground.convert('RGBA'))
+
+    merged_image.save(output_path, format='PNG')
+
+
+crop(["-o", "meow_output.pdf", "-p", "0", "-a", "-5", "textdocx.pdf"])
 
 pdf_file = 'meow_output.pdf'
 output_dir = 'output_images'
 
-pdf_to_images(pdf_path=pdf_file,output_folder="output_images")
+overlay_images("output_images/page1.png", "merged.png")
+
+extract_images_from_pdf(pdf_file, "output_images")
